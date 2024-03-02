25aa4de62f9d70bfbff100ed8d9cefbdb037736c
ubundadu <ptv1324ptv@outlook.com>
2024-03-02 15:31:40 +0530

    Added vignere cipher
    Basic enryption with a key.

--- cypher.py
+++ cypher.py
@@ -1,4 +1,12 @@
 import sys
+'''
+Common functions
+'''
+def call(func,plaintext,key=""):
+    global enc_type
+    return func(plaintext,key)
+
+
 def shift_sub(a,n): #out of bounds check
     ch = chr(ord(a)+n)
     if (a.isupper() and ord(ch)>90) or (a.islower() and ord(ch)>122):
@@ -9,29 +17,87 @@ def shift_sub(a,n): #out of bounds check
         ch = shift_sub(a,0)
     return ch 
 
-def rot13(message):
-    cipher = ""
-    for a in message:
-        if (a.isalpha()): # 78, 110
-            cipher += shift_sub(a,13)
+'''
+rot13 - circularly shifts alphabets by 13
+'''
+def rot13(plaintext,*args): # substitution ciphertext with n=13 rotation
+    ciphertext = ""
+    for a in plaintext:
+        if (a.isalpha()):
+            ciphertext += shift_sub(a,13)
         else:
-            cipher += a
-    return cipher
+            ciphertext += a
+    return ciphertext
 
-def sse(message): # succesive shift encoder
-    cipher = ""
+'''
+sse - succesively changes the shift amount by 1
+'''
+def sse(plaintext,*args): # succesive shift encoder
+    ciphertext = ""
     i = 0
-    for a in message:
+    for a in plaintext:
         if (a.isalpha()): 
-            cipher += shift_sub(a,i)
+            ciphertext += shift_sub(a,i)
+            i += 1 # shift amount
+        else:
+            ciphertext += a
+    return ciphertext
+
+def sse_d(ciphertext,*args): # succesive shift decoder 
+    plaintext = ""
+    i = 0
+    for a in ciphertext:
+        if (a.isalpha()): 
+            plaintext += shift_sub(a,-i)
             i += 1
         else:
-            cipher += a
-        
-    return cipher
+            plaintext += a
+    return plaintext
 
-if (len(sys.argv)>1):
-    message = sys.argv[1] 
-    print("Your encrypted text :\n", sse(message) ,'\n\nFor :\n', rot13(rot13(message)), sep='')
-else:
-    print('Give input block at cmd line')
+'''
+vignere - uses a lookup table and a key 
+'''
+
+def vignere(plaintext,key): # encrypted using a look up table
+    ciphertext = ""
+    rkey = key 
+    if(len(key)<len(plaintext)): #key repetition
+        rkey = key*(len(plaintext)//len(key))
+
+    i = 0
+    for a in plaintext:
+        if (a.isalpha()):
+            k = rkey[i]
+            i+=1
+            ciphertext += shift_sub(a,ord(k)- (65 if k.isupper() else 97))
+        else:
+            ciphertext += a
+    return ciphertext
+
+def vignere_d(ciphertext,key): # decrypted using a look up table
+    plaintext = ""
+    rkey = key 
+    if(len(key)<len(ciphertext)):
+        rkey = key*(len(ciphertext)//len(key))
+
+    i = 0
+    for a in ciphertext:
+        if (a.isalpha()):
+            k = rkey[i]
+            i+=1
+            plaintext += shift_sub(a,-(ord(k)- (65 if k.isupper() else 97)))
+        else:
+            plaintext += a
+    return plaintext
+'''
+entry point
+'''
+if __name__ == "__main__":
+    if (len(sys.argv)>1):
+        plaintext = sys.argv[1] 
+        key = sys.argv[2] if len(sys.argv)>2 else 'abc'
+        ciphertext = call(vignere,plaintext,key) 
+        recovered = call(vignere_d,ciphertext,key)
+        print("Ciphertext :\n", ciphertext ,'\n\nRecovered :\n', recovered)
+    else:
+        print('Give input block at cmd line')
