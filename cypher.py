9fad1e90b770ab2ee7fd2ce33858d07659ee4cd7
ubundadu <ptv1324ptv@outlook.com>
2024-03-02 13:56:26 +0530

    Added SSE (Successive shift encryption)
    Shifting number changes based on location of character

--- cypher.py
+++ cypher.py
@@ -1,30 +1,37 @@
 import sys
-def sub(a,n): #out of bounds check
+def shift_sub(a,n): #out of bounds check
     ch = chr(ord(a)+n)
     if (a.isupper() and ord(ch)>90) or (a.islower() and ord(ch)>122):
         ch = chr(ord(ch)-26)
     elif (a.isupper() and ord(ch)<65) or (a.islower() and ord(ch)<97):
         ch = chr(ord(ch)+26)
+    if not ch.isalpha():
+        ch = shift_sub(a,0)
     return ch 
 
 def rot13(message):
     cipher = ""
     for a in message:
-        
         if (a.isalpha()): # 78, 110
-            cipher += sub(a,13)
+            cipher += shift_sub(a,13)
         else:
             cipher += a
     return cipher
 
 def sse(message): # succesive shift encoder
     cipher = ""
-    for i,a in enumerate(message):
-       pass 
+    i = 0
+    for a in message:
+        if (a.isalpha()): 
+            cipher += shift_sub(a,i)
+            i += 1
+        else:
+            cipher += a
+        
     return cipher
 
 if (len(sys.argv)>1):
     message = sys.argv[1] 
-    print("Your encrypted text :\n", rot13(message) ,'\nFor :', rot13(rot13(message)))
+    print("Your encrypted text :\n", sse(message) ,'\n\nFor :\n', rot13(rot13(message)), sep='')
 else:
     print('Give input block at cmd line')
